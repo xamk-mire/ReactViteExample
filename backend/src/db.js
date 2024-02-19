6774388a4519c1e04a29c88739af6ba145cd34fd
xamk-mire <141403743+xamk-mire@users.noreply.github.com>
2024-02-19 10:02:08 +0200

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,1 @@
-import pg from 'pg';
-const { Pool } = pg;
 
-const pool = new Pool({
-    user: "postgres",
-    password: "Hd6470M",
-    host: "localhost",
-    port: 5432,
-    database: "testDB"
-});
-
-export default pool;
